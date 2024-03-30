75eae697097d0eeb6028b82c82a4bdab5b56c072
ddhairya <d_dhairya@yahoo.com>
2024-03-30 11:03:32 +0300

    feat core docker-compose initial setup

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,19 @@ const reds = require('redis')
 
 const app = express()
 
-const client = reds.createClient()
+const client = reds.createClient({
+    host: 'redis-server',
+    port: 6379
+})
 client.set('visits', 0)
 
 app.get('/', (req,res) =>{
     client.get('visits', (err, visits)=>{
-        res.send(`No of visits : ${{visits}}`)
+        res.send(`No of visits : ${visits}`)
         client.set('visits', parseInt(visits)+1)
     })
 })
 
-app.listen(8080, () => {
+app.listen(8081, () => {
     console.log("Server is running at port 8080")
 })
